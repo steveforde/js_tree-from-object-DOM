8228d4d1db142a43cea20ca078b4bd9e7bba34d0
Stephen Forde <sforde08@gmail.com>
2023-12-19 13:45:11 +0000

    make build

---
+++ index.01a027c1.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,"object"==typeof n[t]&&Object.keys(n[t]).length>0&&e(l,n[t]),r.append(l)}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.01a027c1.js.map
